933a6a2149a38e31bc855ffba40965e87a9a9967
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2020-12-09 08:03:20 +0100

    almost done

--- api/controllers/MailController.js
+++ api/controllers/MailController.js
@@ -6,9 +6,44 @@
  */
 
 module.exports = {
-    // send: (mailDetails) => {
-    //     // that whole long ass function
-    //    }
-
+        send: (mailDetails) => {
+        
+        var nodemailer = require('nodemailer');
+        var hbs = require('nodemailer-express-handlebars');
+        var transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: ' ',
+                pass: ' '
+            }
+        });
+        transporter.use('compile', hbs({
+            viewEngine: {
+                extName: ".hbs",
+                partialsDir: "./views",
+                layoutsDir: "./views",
+                defaultLayout: "",
+            },
+            viewPath: "./views/",
+            extName: ".hbs",
+        }));
+        
+         var mailOptions = {
+            from: 'Hassan Ademuyiwa ✔ <ademuyiwahassan68@gmail.com>',
+            ...mailDetails
+         }
+        
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log(error);
+            }else{
+                console.log('Message sent: ' + info.response);
+            }
+        });
+        
+        
+        
+        }
 };
+
 
